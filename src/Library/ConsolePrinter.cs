cd8b95997e1ba859faebc5497ea239a93e045c6d
Sebastian Forische <sebastian.forische@correo.ucu.edu.uy>
2021-09-25 16:55:49 -0300

    ejercicio terminado

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -3,7 +3,8 @@ using System.IO;
 
 namespace Full_GRASP_And_SOLID.Library
 {
-    // esta operacion  es polimórfica porque es implementada por dos o más objetos de diferentes tipos.
+    
+    
     public class ConsolePrinter : IPrinter
 {
     public void PrintTicket(Recipe receta)
